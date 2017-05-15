01a8f6652d645287d52de2402f31cae43c3c447f
Ramon <Rahmon@users.noreply.github.com>
2017-05-15 11:00:48 -0300

    Initial files

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * anatta Theme Customizer
+ *
+ * @package anatta
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function anatta_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'anatta_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function anatta_customize_preview_js() {
+	wp_enqueue_script( 'anatta_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'anatta_customize_preview_js' );
