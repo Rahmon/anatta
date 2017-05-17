799240960bd50f0904780b228bd2fabd603b336d
Ramon <Rahmon@users.noreply.github.com>
2017-05-17 14:10:03 -0300

    Adjusts on anatta_posted_on function

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -14,7 +14,7 @@ if ( ! function_exists( 'anatta_posted_on' ) ) :
 function anatta_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>' . esc_html__(' and Updated on ', 'anatta') . '<time class="updated" datetime="%3$s">%4$s</time>';
 	}
 
 	$time_string = sprintf( $time_string,
@@ -25,16 +25,14 @@ function anatta_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'anatta' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
+		esc_html_x( 'Posted on %s', 'post date', 'anatta' ), $time_string );
 
 	$byline = sprintf(
 		esc_html_x( 'by %s', 'post author', 'anatta' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<p class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</p>'; // WPCS: XSS OK.
 
 }
 endif;
