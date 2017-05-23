a1bac0d9cdafa909886ba553eafa734810291883
Ramon <Rahmon@users.noreply.github.com>
2017-05-23 18:01:24 -0300

    Disable support to post-thumbnail

--- functions.php
+++ functions.php
@@ -35,13 +35,6 @@ function anatta_setup() {
 	 */
 	add_theme_support( 'title-tag' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
-	add_theme_support( 'post-thumbnails' );
-
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'menu-1' => esc_html__( 'Primary', 'anatta' ),
