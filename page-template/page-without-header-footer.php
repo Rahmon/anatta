5624a653c05887e0f5f8399dad5c502fda1b6214
Ramon <Rahmon@users.noreply.github.com>
2017-05-23 17:51:21 -0300

    Add simple page template

--- page-template/page-without-header-footer.php
+++ page-template/page-without-header-footer.php
@@ -1,6 +1,25 @@
-<?php 
+<?php
 /*
-Template Name: Page Without Header and Footer
+Template Name: Simple Page
 Template Post Type: page
 */
-// Page code here...
+get_header( 'simple-page' );
+?>
+
+    <main id="main" class="site-main" role="main">
+      <article>
+        <?php
+          if ( have_posts() ) :
+          	while ( have_posts() ) : the_post();
+          		the_content();
+          	endwhile;
+          endif;
+        ?>
+      </article>
+    </main>
+  </div><!-- #content -->
+  <footer></footer>
+
+<?php wp_footer() ?>
+</body>
+</html>
