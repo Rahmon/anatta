d1327d3950b9d65c846b8f6850639c38d3545b2e
Ramon <Rahmon@users.noreply.github.com>
2017-05-29 09:38:50 -0300

    Update post navigation

--- single.php
+++ single.php
@@ -16,9 +16,19 @@ get_header(); ?>
 		while ( have_posts() ) : the_post();
 
 			get_template_part( 'template-parts/content', get_post_format() );
-
-			the_post_navigation();
-
+		?>
+			<nav class="navigation post-navigation" role="navigation">
+				<h2 class="screen-reader-text"><?php esc_html_e( 'Post navigation', 'anatta' ); ?></h2>
+				<div class="nav-links">
+					<div class="nav-previous">
+						<?php previous_post_link( '<span class="meta-nav">←</span> %link' ); ?>
+					</div>
+					<div class="nav-next">
+						<?php next_post_link( '%link <span class="meta-nav">→</span>' ); ?>
+					</div>
+				</div>
+			</nav>
+		<?php
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
