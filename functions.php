f8cd586d46a2eed8a43ad52c719d63dbcf6d986f
Rahmon <Rahmon@users.noreply.github.com>
2017-11-23 10:17:33 -0200

    remove custom header

--- functions.php
+++ functions.php
@@ -97,11 +97,6 @@ function anatta_scripts() {
 add_action( 'wp_enqueue_scripts', 'anatta_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
